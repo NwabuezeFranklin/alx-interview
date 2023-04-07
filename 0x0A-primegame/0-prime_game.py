4cfa0769981a26b02c623cb781899e19e12aa7ec
NwabuezeFranklin <nwafrank2017@gmail.com>
2023-04-06 19:54:36 -0700

    Ben wins fixed

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,29 +1,52 @@
 #!/usr/bin/python3
 '''Prime game module.
 '''
-
-
 def isWinner(x, nums):
-    '''Determines the winner from an array of game played
-    '''
-    if x < 1 or not nums:
-        return None
-    maria, ben = 0, 0
+    def is_prime(num):
+        if num < 2:
+            return False
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
 
-    max_n = max(nums)
-    primes = [True for _ in range(1, max_n + 1, 1)]
-    primes[0] = False
-    for i, is_prime in enumerate(primes, 1):
-        if i == 1 or not is_prime:
-            continue
-        for j in range(i + i, max_n + 1, i):
-            primes[j - 1] = False
+    def get_primes(n):
+        primes = []
+        for i in range(2, n + 1):
+            if is_prime(i):
+                primes.append(i)
+        return primes
 
-    for _, n in zip(range(x), nums):
-        primes_count = len(list(filter(lambda x: x, primes[0: n])))
-        ben += primes_count % 2 == 0
-        maria += primes_count % 2 == 1
-    if maria == ben:
+    def play_game(n):
+        primes = get_primes(n)
+        maria_turn = True
+        while primes:
+            if maria_turn:
+                for prime in primes:
+                    if n % prime == 0:
+                        primes.remove(prime)
+                        break
+                else:
+                    return "Ben"
+            else:
+                for prime in primes:
+                    if n % prime == 0:
+                        primes.remove(prime)
+                        break
+                else:
+                    return "Maria"
+            maria_turn = not maria_turn
+        return "Maria"
+
+    winners = {"Maria": 0, "Ben": 0}
+    for n in nums:
+        winner = play_game(n)
+        winners[winner] += 1
+
+    if winners["Maria"] > winners["Ben"]:
+        return "Maria"
+    elif winners["Ben"] > winners["Maria"]:
+        return "Ben"
+    else:
         return None
-    return 'Maria' if maria > ben else 'Ben'
 
